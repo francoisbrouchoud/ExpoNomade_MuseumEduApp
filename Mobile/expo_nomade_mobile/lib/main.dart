a009a5998fbd1c046c2417c6584eb716762b33fa
Celinevialard <celine.vialard@gmail.com>
2023-07-23 13:28:25 +0200

    add firebase into project

--- Mobile/expo_nomade_mobile/lib/main.dart
+++ Mobile/expo_nomade_mobile/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'home_page.dart';
 import 'map/map_page.dart';
 import 'app_localization.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
