dc197243bef649776a6e282ced30d043b6e7fea8
LauMey <laurian.meynet@students.hevs.ch>
2023-08-31 18:20:23 +0200

    Objects on map from BO

--- Mobile/expo_nomade_mobile/lib/map/info_panel.dart
+++ Mobile/expo_nomade_mobile/lib/map/info_panel.dart
@@ -1,18 +1,22 @@
+import 'package:expo_nomade_mobile/app_localization.dart';
+import 'package:expo_nomade_mobile/bo/expo_object.dart';
 import 'package:flutter/material.dart';
 
 /// Class InfoPanel is used to display information about a marker.
 class InfoPanel extends StatelessWidget {
-  final String markerId;
+  final ExpoObject object;
   final VoidCallback onClose;
 
   const InfoPanel({
     Key? key,
-    required this.markerId,
+    required this.object,
     required this.onClose,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final translations = AppLocalization.of(context);
+    final langCode = translations.getCurrentLangCode();
     return Container(
       // TODO set theme bg color
       child: Column(
@@ -26,7 +30,11 @@ class InfoPanel extends StatelessWidget {
               ),
             ],
           ),
-          Text('Marker id: $markerId'),
+          //Text('Marker id: $markerId'),
+          // ICI LE TEXTE DOIT ÊTRE TABLEAUDOBJET[NUMéROD'INDEX].TITLE[LANGCODE]
+
+          Text(object.title[langCode]),
+          Text(object.description[langCode])
           // TODO add info contained in marker!
           // TODO ensure this container is scrollable
         ],
