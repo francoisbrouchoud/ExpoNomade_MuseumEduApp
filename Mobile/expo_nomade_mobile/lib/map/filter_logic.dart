ad773c8cae427ff05bb5f7cf302b3d4497f788cc
zacharie.renna <zacharie.renna@students.hevs.ch>
2023-09-01 09:19:45 +0200

    Fixed issue with objects year filter

--- Mobile/expo_nomade_mobile/lib/map/filter_logic.dart
+++ Mobile/expo_nomade_mobile/lib/map/filter_logic.dart
@@ -3,30 +3,41 @@ import 'dart:collection';
 import '../bo/expo_event.dart';
 import '../bo/expo_object.dart';
 
-List<ExpoEvent> filterEventsByYear(List<ExpoEvent> events, double startYear, double endYear) {
+/// Filters a list of ExpoEvent by year.
+List<ExpoEvent> filterEventsByYear(
+    List<ExpoEvent> events, double startYear, double endYear) {
   return events.where((event) {
     return event.startYear >= startYear && event.endYear <= endYear;
   }).toList();
 }
 
-Map<ExpoObject, int> filterObjectsByYear(List<ExpoObject> objs, double startYear, double endYear) {
-    Map<ExpoObject, int> filtered = HashMap();
-    for (ExpoObject obj in objs) {
-        int? y;
-        List<int> potentialY = obj.coordinates.keys.where((yr) { return yr >= startYear && yr <= endYear; }).toList();
-        if (potentialY.isNotEmpty) {
-            for (int year in potentialY) {
-                if (y == null) {
-                    y = year;
-                }
-                else if (year > y) {
-                    y = year;
-                }
-            }
-        }
-        if (y != null) {
-            filtered.putIfAbsent(obj, () => y!);
-        }
+/// Filters a list of ExpoObject by year.
+Map<ExpoObject, int> filterObjectsByYear(
+    List<ExpoObject> objs, double startYear, double endYear) {
+  Map<ExpoObject, int> filtered = HashMap();
+  for (ExpoObject obj in objs) {
+    int? y;
+    List<int> potentialY = obj.coordinates.keys.where((yr) {
+      return yr >= startYear && yr <= endYear;
+    }).toList();
+    // if no year is between the selected dates, we want to display the most recent one
+    if (potentialY.isEmpty) {
+      potentialY = obj.coordinates.keys.where((yr) {
+        return startYear >= yr && endYear >= yr;
+      }).toList();
     }
-    return filtered;
+    if (potentialY.isNotEmpty) {
+      for (int year in potentialY) {
+        if (y == null) {
+          y = year;
+        } else if (year > y) {
+          y = year;
+        }
+      }
+    }
+    if (y != null) {
+      filtered.putIfAbsent(obj, () => y!);
+    }
+  }
+  return filtered;
 }
