0dda680222739548551fe8e94270cb60d0620d81
Celinevialard <celine.vialard@gmail.com>
2023-09-01 14:04:36 +0200

    update container widget

--- Mobile/expo_nomade_mobile/lib/util/container_widget.dart
+++ Mobile/expo_nomade_mobile/lib/util/container_widget.dart
@@ -11,21 +11,24 @@ class ContainerWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     double screenWidth = MediaQuery.of(context).size.width;
-    return Center(
-        child: Column(children: [
-      const SizedBox(height: 60),
-      TitleWidget(text: title),
-      const SizedBox(height: 30),
-      Container(
-          width: screenWidth * 0.7,
-          margin: EdgeInsets.symmetric(vertical: 8),
-          padding: EdgeInsets.all(12.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16.0),
-            color: theme.colorScheme.background,
-          ),
-          child: Center(
-              child: Padding(padding: const EdgeInsets.all(10), child: body)))
-    ]));
+    return Scaffold(
+        backgroundColor: theme.colorScheme.primary,
+        body: Center(
+            child: Column(children: [
+          const SizedBox(height: 60),
+          TitleWidget(text: title),
+          const SizedBox(height: 30),
+          Container(
+              width: screenWidth * 0.7,
+              margin: EdgeInsets.symmetric(vertical: 8),
+              padding: EdgeInsets.all(12.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16.0),
+                color: theme.colorScheme.background,
+              ),
+              child: Center(
+                  child:
+                      Padding(padding: const EdgeInsets.all(10), child: body)))
+        ])));
   }
 }
