17a6b15f2c553aef3868ba4fe50b9aeb82afaf43
zacharie.renna <zacharie.renna@students.hevs.ch>
2023-09-04 18:03:03 +0200

    Fixed issue: non-mandatory elements are now saved in db even if empty

--- Mobile/expo_nomade_mobile/lib/util/multilingual_string.dart
+++ Mobile/expo_nomade_mobile/lib/util/multilingual_string.dart
@@ -1,3 +1,6 @@
+import 'package:expo_nomade_mobile/app_localization.dart';
+import 'package:expo_nomade_mobile/util/validation_helper.dart';
+
 /// Class MultilingualString is used to manage translated properties.
 class MultilingualString {
   Map<String, String> translations;
@@ -7,11 +10,19 @@ class MultilingualString {
 
   /// Returns the translation in the language provided by the lang code. If no translation exists for the lang code, an empty string will be returned.
   String operator [](String langCode) {
-    return translations[langCode] ?? "";
+    String val = translations[langCode] ?? "";
+    if (val.isNotEmpty && isEmpty(val)) {
+      val = "";
+    }
+    return val;
   }
 
   /// Returns all the values of the MultilingualString in a Map format.
   Map<String, String> toMap() {
-    return translations;
+    if (translations.isNotEmpty) {
+      return translations;
+    } else {
+      return {for (var lang in Language.langList()) lang.langCode: emptyString};
+    }
   }
 }
--- Mobile/expo_nomade_mobile/lib/util/validation_helper.dart
+++ Mobile/expo_nomade_mobile/lib/util/validation_helper.dart
@@ -3,3 +3,8 @@ bool isValidEmail(String email) {
       r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
   return emailRegex.hasMatch(email);
 }
+
+const emptyString = "EMPTY";
+bool isEmpty(String string) {
+  return string == emptyString;
+}
