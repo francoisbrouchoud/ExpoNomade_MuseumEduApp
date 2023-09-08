ea9d0373381d45eeaa894b3946263586576779d3
LauMey <laurian.meynet@students.hevs.ch>
2023-09-08 11:51:33 +0200

    Beautiful Info Display

--- Mobile/expo_nomade_mobile/lib/map/info_panel_event.dart
+++ Mobile/expo_nomade_mobile/lib/map/info_panel_event.dart
@@ -1,6 +1,7 @@
 import 'package:expo_nomade_mobile/app_localization.dart';
 import 'package:expo_nomade_mobile/bo/expo_event.dart';
 import 'package:flutter/material.dart';
+import 'package:expo_nomade_mobile/util/globals.dart';
 
 class InfoPanelEvent extends StatelessWidget {
   final ExpoEvent event;
@@ -14,13 +15,15 @@ class InfoPanelEvent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    final theme = Theme.of(context);
     final translations = AppLocalization.of(context);
     final langCode = translations.getCurrentLangCode();
 
     return Container(
-      color: Colors.white, // Set your desired background color
-      margin: EdgeInsets.all(16.0), // Add margins
-      padding: EdgeInsets.all(16.0), // Add internal padding
+      height: screenHeight,
+      color: theme.colorScheme.primary, // Set your desired background color
+      padding: const EdgeInsets.all(32), // Add internal padding
       child: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -34,15 +37,61 @@ class InfoPanelEvent extends StatelessWidget {
                 ),
               ],
             ),
-            SizedBox(height: 20), // Add spacing
+            const SizedBox(
+                height: GlobalConstants.infoPanelsSmallSpacing), // Add spacing
             Text(
               event.title[langCode],
-              style: TextStyle(
+              style: const TextStyle(
                 fontSize: 24, // Increase font size
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 10), // Add spacing
+            const SizedBox(
+                height: GlobalConstants.infoPanelsSmallSpacing), // Add spacing
+            if (event.pictureURL != '')
+              SizedBox(
+                child: Image.network(event.pictureURL),
+                height: GlobalConstants.imagesDefaultDimension,
+              ),
+
+            const SizedBox(
+                height: GlobalConstants.infoPanelsMediumSpacing), // Add spacing
+            Row(
+              children: [
+                Text(
+                  '${translations.getTranslation('reason')} : ',
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(event.reason[langCode]),
+              ],
+            ),
+
+            const SizedBox(
+                height: GlobalConstants.infoPanelsSmallSpacing), // Add spacing
+            Row(
+              children: [
+                Text(
+                  '${translations.getTranslation('population_type')} : ',
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(event.populationType.title[langCode]),
+              ],
+            ),
+
+            const SizedBox(
+                height: GlobalConstants.infoPanelsSmallSpacing), // Add spacing
+            Row(children: [
+              Text(
+                '${translations.getTranslation('years')} : ',
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(event.startYear.toString()),
+              Text(" - "),
+              Text(event.endYear.toString()),
+            ]),
+
+            const SizedBox(
+                height: GlobalConstants.infoPanelsSmallSpacing), // Add spacing
             Text(event.description[langCode]),
           ],
         ),
