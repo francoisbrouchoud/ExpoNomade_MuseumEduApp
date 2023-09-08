5941eb6aebd266246a4dcbe93dec7dc7095ed911
zacharie.renna <zacharie.renna@students.hevs.ch>
2023-09-08 13:30:39 +0200

    Updated algorithm to detect if tap is on polygon

--- Mobile/expo_nomade_mobile/lib/map/helper_map.dart
+++ Mobile/expo_nomade_mobile/lib/map/helper_map.dart
@@ -1,5 +1,4 @@
 import 'dart:collection';
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -21,43 +20,25 @@ Map<ExpoEvent, Polygon> generatePolygone(
   return eventPoly;
 }
 
-/// detect if a point is in a polygon
+/// Detect if a point is in a polygon
 bool pointInPolygon(LatLng position, Polygon polygon) {
-  // Check if the point is inside the polygon or on the boundary
-  int intersections = 0;
+  bool isInside = false;
   var verticesCount = polygon.points.length;
 
-  for (int i = 1; i < verticesCount; i++) {
-    LatLng vertex1 = polygon.points[i - 1];
-    LatLng vertex2 = polygon.points[i];
+  for (int i = 0, j = verticesCount - 1; i < verticesCount; j = i++) {
+    LatLng vertex1 = polygon.points[i];
+    LatLng vertex2 = polygon.points[j];
 
-    // Check if point is on an horizontal polygon boundary
-    if (vertex1.latitude == vertex2.latitude &&
-        vertex1.latitude == position.latitude &&
-        position.longitude > min(vertex1.longitude, vertex2.longitude) &&
-        position.longitude < max(vertex1.longitude, vertex2.longitude)) {
-      return true;
-    }
-
-    if (position.latitude > min(vertex1.latitude, vertex2.latitude) &&
-        position.latitude <= max(vertex1.latitude, vertex2.latitude) &&
-        position.longitude <= max(vertex1.longitude, vertex2.longitude) &&
-        vertex1.latitude != vertex2.latitude) {
-      var xinters = (position.latitude - vertex1.latitude) *
-              (vertex2.longitude - vertex1.longitude) /
-              (vertex2.latitude - vertex1.latitude) +
-          vertex1.longitude;
-      if (xinters == position.longitude) {
-        // Check if point is on the polygon boundary (other than horizontal)
-        return true;
-      }
-      if (vertex1.longitude == vertex2.longitude ||
-          position.longitude <= xinters) {
-        intersections++;
-      }
+    if (((vertex1.latitude > position.latitude) !=
+            (vertex2.latitude > position.latitude)) &&
+        (position.longitude <
+            (vertex2.longitude - vertex1.longitude) *
+                    (position.latitude - vertex1.latitude) /
+                    (vertex2.latitude - vertex1.latitude) +
+                vertex1.longitude)) {
+      isInside = !isInside;
     }
   }
 
-  // If the number of edges we passed through is odd, then it's in the polygon.
-  return intersections % 2 != 0;
+  return isInside;
 }
