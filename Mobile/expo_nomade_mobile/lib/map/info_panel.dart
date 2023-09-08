f98200da052c1499f13befa9974fa1c46d270b66
Celinevialard <celine.vialard@gmail.com>
2023-09-08 12:06:41 +0200

    use text them info panel

--- Mobile/expo_nomade_mobile/lib/map/info_panel.dart
+++ Mobile/expo_nomade_mobile/lib/map/info_panel.dart
@@ -19,6 +19,13 @@ class InfoPanel extends StatelessWidget {
     final theme = Theme.of(context);
     final translations = AppLocalization.of(context);
     final langCode = translations.getCurrentLangCode();
+    final titleStyle = theme.textTheme.displayMedium!.copyWith(
+      fontWeight: FontWeight.bold,
+    );
+    final textStyle = theme.textTheme.displaySmall!;
+    final labelStyle = theme.textTheme.displaySmall!.copyWith(
+      fontWeight: FontWeight.bold,
+    );
 
     return Container(
       height: screenHeight,
@@ -42,10 +49,7 @@ class InfoPanel extends StatelessWidget {
                 height: GlobalConstants.infoPanelsSmallSpacing), // Add spacing
             Text(
               object.title[langCode],
-              style: const TextStyle(
-                fontSize: 24, // Increase font size
-                fontWeight: FontWeight.bold,
-              ),
+              style: titleStyle,
             ),
             const SizedBox(
                 height: GlobalConstants.infoPanelsSmallSpacing), // Add spacing
@@ -60,9 +64,10 @@ class InfoPanel extends StatelessWidget {
               children: [
                 Text(
                   '${translations.getTranslation('year')} : ',
-                  style: const TextStyle(fontWeight: FontWeight.bold),
+                  style: labelStyle,
                 ),
-                Text(object.coordinates.keys.first.toString()),
+                Text(object.coordinates.keys.first.toString(),
+                    style: textStyle),
               ],
             ),
 
@@ -72,9 +77,9 @@ class InfoPanel extends StatelessWidget {
               children: [
                 Text(
                   '${translations.getTranslation('museum')} : ',
-                  style: const TextStyle(fontWeight: FontWeight.bold),
+                  style: labelStyle,
                 ),
-                Text(object.museum.name[langCode]),
+                Text(object.museum.name[langCode], style: textStyle),
               ],
             ),
 
@@ -84,9 +89,9 @@ class InfoPanel extends StatelessWidget {
               children: [
                 Text(
                   '${translations.getTranslation('position')} : ',
-                  style: const TextStyle(fontWeight: FontWeight.bold),
+                  style: labelStyle,
                 ),
-                Text(object.position[langCode]),
+                Text(object.position[langCode], style: textStyle),
               ],
             ),
 
@@ -96,9 +101,9 @@ class InfoPanel extends StatelessWidget {
               children: [
                 Text(
                   '${translations.getTranslation('dimensions')} : ',
-                  style: const TextStyle(fontWeight: FontWeight.bold),
+                  style: labelStyle,
                 ),
-                Text(object.dimension),
+                Text(object.dimension, style: textStyle),
               ],
             ),
 
@@ -108,14 +113,14 @@ class InfoPanel extends StatelessWidget {
               children: [
                 Text(
                   '${translations.getTranslation('material')} : ',
-                  style: const TextStyle(fontWeight: FontWeight.bold),
+                  style: labelStyle,
                 ),
-                Text(object.material[langCode]),
+                Text(object.material[langCode], style: textStyle),
               ],
             ),
 
             const SizedBox(height: 20), // Add spacing
-            Text(object.description[langCode]),
+            Text(object.description[langCode], style: textStyle),
           ],
         ),
       ),
